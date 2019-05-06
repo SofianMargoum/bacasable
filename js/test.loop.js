50e49b11afc069c2778b302f94aba2756719bb08
Philippe <philippe@henaff.me>
2019-05-06 18:39:12 +0200

    Update test.loop.js

--- js/test.loop.js
+++ js/test.loop.js
@@ -1,1 +1,11 @@
-for (let i = 1 ; i < 21 ; i++) { console.log(i) }
+/*for (let i = 1 ; i < 21 ; i++) { console.log(i) }*/
+
+var chats = ['Bill', 'Jeff', 'Pete', 'Biggles', 'Jasmin'];
+var info = 'Mes chat s appellent ';
+
+
+for (var i = 0; i < chats.length; i++) {
+  info += chats[i] + ', ';
+}
+
+console.log(info);
