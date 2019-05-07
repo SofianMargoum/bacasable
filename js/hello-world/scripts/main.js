e589cd6c2640ccc9ce6b6fb7315447cb7905213a
Philippe <philippe@henaff.me>
2019-05-07 06:58:33 +0200

    JS Hello world

--- js/hello-world/scripts/main.js
+++ js/hello-world/scripts/main.js
@@ -1,7 +1,17 @@
 //https://developer.mozilla.org/fr/docs/Apprendre/Commencer_avec_le_web/Les_bases_JavaScript
 
-let myImage = document.querySelector('img');
+/**
+ * 
+ 
 
+ let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Bonjour, monde !';
+*/
+/**
+ * 
+ */
+
+let myImage = document.querySelector('img');
 myImage.addEventListener('click', function() {
     let mySrc = myImage.getAttribute('src');
     if (mySrc === 'images/firefox-icon.png') {
@@ -10,3 +20,28 @@ myImage.addEventListener('click', function() {
       myImage.setAttribute('src', 'images/firefox-icon.png');
     }
 });
+
+/**
+ * 
+
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Veuillez saisir votre nom.');
+  localStorage.setItem('nom', myName);
+  myHeading.textContent = 'Mozilla est cool, ' + myName;
+}
+
+if (!localStorage.getItem('nom')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('nom');
+  myHeading.textContent = 'Mozilla est cool, ' + storedName;
+}
+
+myButton.addEventListener('click', function() {
+  setUserName();
+});
+ */
