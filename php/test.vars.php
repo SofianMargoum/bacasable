34dd21eb4fe28610e767aef7074c5bc0c4b5fa36
Philippe <philippe@henaff.me>
2019-05-16 17:54:08 +0200

    Ajout branche php

---
+++ php/test.vars.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Structure de base HTML 5</title>
+    </head>
+
+    <body>
+    
+        <p>$a = array(1, 2, array("a", "b", "c"));</p>
+
+        <?php
+        
+        var_dump($unset_var);
+
+        $a = array(1, 2, array("a", "b", "c"));
+        ?>
+
+        <p>var_dump($a)</p>
+        <pre>
+        <?php
+        var_dump($a);
+        ?>
+        </pre>
+        
+        <p>print_r($a)</p>
+        <pre>
+        <?php
+        print_r($a);
+        ?>
+        </pre>
+    </body>
+</html>
